1817773b33481f35f6e45a5dfb863eea534186bf
Rupert Steel <rupert@rupertsteel.me>
2022-12-02 23:25:34 +1300

    Day 2 part 2

--- day2/main.cpp
+++ day2/main.cpp
@@ -12,6 +12,10 @@
 
 using namespace std::string_view_literals;
 
+auto positiveMod(auto a, auto b) {
+	return (a % b + b) % b;
+}
+
 int main(int argc, char* argv[]) {
 	std::ifstream inputFile("inputs/day2.txt");
 	std::string input(std::istreambuf_iterator{ inputFile }, std::istreambuf_iterator<char>{});
@@ -29,7 +33,7 @@ int main(int argc, char* argv[]) {
 		return std::make_pair(left, right);
 	});
 
-	auto calcGameScoresFollowingStrategy = inputIntoGamePairs | std::views::transform([](std::pair<int, int> game) {
+	auto calcScoreFunc = std::views::transform([](std::pair<int, int> game) {
 		bool isDraw = game.first == game.second;
 
 		bool isWin = (game == std::make_pair(1, 2)) || (game == std::make_pair(2, 3)) || (game == std::make_pair(3, 1));
@@ -37,9 +41,25 @@ int main(int argc, char* argv[]) {
 		return game.second + (isDraw ? 3 : 0) + (isWin ? 6 : 0);
 	});
 
+	auto calcGameScoresFollowingStrategy = inputIntoGamePairs | calcScoreFunc;
+
 	auto totalScore = std::accumulate(calcGameScoresFollowingStrategy.begin(), calcGameScoresFollowingStrategy.end(), 0);
 
-	fmt::print("Part 1, Score: {}", totalScore);
+	fmt::print("Part 1, Score: {}\n", totalScore);
+
+	auto inputIntoGamePairs2 = inputIntoLines | std::views::transform([](std::string_view sv) {
+		auto left = sv[0] - 'A' + 1;
+
+		auto rightChange = sv[2] - 'X' - 1;
+
+		return std::make_pair(left, positiveMod(left - 1 + rightChange, 3) + 1);
+	});
+
+	auto calcGameScoresFollowingStrategy2 = inputIntoGamePairs2 | calcScoreFunc;
+
+	auto totalScore2 = std::accumulate(calcGameScoresFollowingStrategy2.begin(), calcGameScoresFollowingStrategy2.end(), 0);
+
+	fmt::print("Part 2, Score: {}\n", totalScore2);
 
 	return 0;
 }
--- tests/testMain.cpp
+++ tests/testMain.cpp
@@ -1,2 +1,28 @@
 
 #include "catch2/catch_test_macros.hpp"
+
+auto positiveMod(auto a, auto b) {
+	return (a % b + b) % b;
+}
+
+TEST_CASE("Day 2 part 2, change hand", "") {
+	auto func = [](char leftIn, char rightIn) {
+		auto left = leftIn - 'A' + 1;
+
+		auto rightChange = rightIn - 'X' - 1;
+
+		return positiveMod(left - 1 + rightChange, 3) + 1;
+	};
+
+	CHECK(func('A', 'X') == 3);
+	CHECK(func('B', 'X') == 1);
+	CHECK(func('C', 'X') == 2);
+
+	CHECK(func('A', 'Y') == 1);
+	CHECK(func('B', 'Y') == 2);
+	CHECK(func('C', 'Y') == 3);
+
+	CHECK(func('A', 'Z') == 2);
+	CHECK(func('B', 'Z') == 3);
+	CHECK(func('C', 'Z') == 1);
+}
