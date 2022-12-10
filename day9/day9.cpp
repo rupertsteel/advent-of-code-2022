7f4e90ac47593506dc012a5da68cd022a05d0dfe
Rupert Steel <rupert@rupertsteel.me>
2022-12-10 19:24:52 +1300

    Day 9 part 1

---
+++ day9/day9.cpp
@@ -0,0 +1,113 @@
+
+#include <fmt/format.h>
+#include <iostream>
+#include <fstream>
+#include <ranges>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <chrono>
+#include <regex>
+#include <map>
+#include <set>
+#include <unordered_set>
+
+
+#include <fmt/ranges.h>
+#include <fmt/chrono.h>
+
+#include <gsl/gsl-lite.hpp>
+
+using namespace std::string_view_literals;
+
+struct Instruction {
+	int diffX;
+	int diffY;
+	int count;
+};
+
+template<>
+struct std::hash<std::pair<int, int>> {
+	std::size_t operator()(std::pair<int, int> const& s) const noexcept
+	{
+		std::size_t h1 = std::hash<int>{}(s.first);
+		std::size_t h2 = std::hash<int>{}(s.second);
+		return h1 ^ (h2 << 1); // or use boost::hash_combine
+	}
+};
+
+int main(int argc, char* argv[]) {
+	std::ifstream inputFile("inputs/day9.txt");
+	std::string input(std::istreambuf_iterator{ inputFile }, std::istreambuf_iterator<char>{});
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	auto inputIntoInstructions = std::string_view{ input } | std::views::split("\n"sv) | std::views::transform([](auto rng) { return std::string_view(rng.begin(), rng.end()); }) | std::views::filter([](auto str) { return !str.empty(); })
+		| std::views::transform([](auto sv) {
+			Instruction ins;
+
+			switch (sv[0]) {
+			case 'U':
+				ins.diffX = 0;
+				ins.diffY = 1;
+				break;
+			case 'D':
+				ins.diffX = 0;
+				ins.diffY = -1;
+				break;
+			case 'R':
+				ins.diffX = 1;
+				ins.diffY = 0;
+				break;
+			case 'L':
+				ins.diffX = -1;
+				ins.diffY = 0;
+				break;
+			}
+
+			auto countSv = sv.substr(2);
+			std::string countStr{ countSv };
+			auto count = std::stoi(countStr);
+
+			ins.count = count;
+
+			return ins;
+		});
+
+	auto headX = 0;
+	auto headY = 0;
+	auto tailX = 0;
+	auto tailY = 0;
+
+	std::unordered_set<std::pair<int, int>> visitedLocations;
+
+	for (auto instruction : inputIntoInstructions) {
+		for (int i = 0; i < instruction.count; i++) {
+			headX += instruction.diffX;
+			headY += instruction.diffY;
+
+			auto diffX = headX - tailX;
+			auto diffY = headY - tailY;
+
+			auto needMove = std::abs(diffX) > 1 || std::abs(diffY) > 1;
+
+			if (needMove) {
+				tailX += std::clamp(diffX, -1, 1);
+				tailY += std::clamp(diffY, -1, 1);
+			}
+
+			visitedLocations.emplace(tailX, tailY);
+		}
+	}
+
+	auto end = std::chrono::high_resolution_clock::now();
+
+	fmt::print("Part 1, # visited locations {}\n", visitedLocations.size());
+
+
+	auto dur = end - start;
+	fmt::print("Took {}\n", dur);
+
+	return 0;
+}
