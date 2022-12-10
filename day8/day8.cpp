e82846fa8d6dbd717c8ce8122ea54783db3e63e1
Rupert Steel <rupert@rupertsteel.me>
2022-12-10 17:58:03 +1300

    Day 8 part 1

---
+++ day8/day8.cpp
@@ -0,0 +1,136 @@
+
+#include <fmt/format.h>
+#include <iostream>
+#include <fstream>
+#include <ranges>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <chrono>
+#include <regex>
+#include <map>
+
+
+#include <fmt/ranges.h>
+#include <fmt/chrono.h>
+
+#include <gsl/gsl-lite.hpp>
+
+using namespace std::string_view_literals;
+
+struct visability {
+	bool top = false;
+	bool right = false;
+	bool bot = false;
+	bool left = false;
+};
+
+int main(int argc, char* argv[]) {
+	std::ifstream inputFile("inputs/day8.txt");
+	std::string input(std::istreambuf_iterator{ inputFile }, std::istreambuf_iterator<char>{});
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	auto inputIntoLines = std::string_view{ input } | std::views::split("\n"sv) | std::views::transform([](auto rng) { return std::string_view(rng.begin(), rng.end()); }) | std::views::filter([](auto str) { return !str.empty(); });
+
+	std::vector<int> data;
+	data.reserve(input.size());
+	int width;
+	int length = 0;
+
+	for (auto line : inputIntoLines) {
+		length++;
+		width = line.size();
+
+		for (auto ch : line) {
+			data.push_back(ch - '0');
+		}
+	}
+
+	std::vector<visability> visabilityMap(data.size());
+
+	auto arrLookup = [width](auto& vec, int x, int y) -> auto& {
+		auto offset = y * width + x;
+
+		return vec[offset];
+	};
+
+	// top down
+	for (int i = 0; i < width; i++) {
+		int currentHeight = -1;
+
+		for (int j = 0; j < length; j++) {
+			if (arrLookup(data, i, j) > currentHeight) {
+				currentHeight = arrLookup(data, i, j);
+				arrLookup(visabilityMap, i, j).top = true;
+			}
+
+			if (currentHeight == 9) {
+				break;
+			}
+		}
+	}
+
+	// right left
+	for (int i = 0; i < length; i++) {
+		int currentHeight = -1;
+
+		for (int j = width - 1; j >= 0; j--) {
+			if (arrLookup(data, j, i) > currentHeight) {
+				currentHeight = arrLookup(data, j, i);
+				arrLookup(visabilityMap, j, i).right = true;
+			}
+
+			if (currentHeight == 9) {
+				break;
+			}
+		}
+	}
+
+	// bot top
+	for (int i = 0; i < width; i++) {
+		int currentHeight = -1;
+
+		for (int j = length - 1; j >= 0; j--) {
+			if (arrLookup(data, i, j) > currentHeight) {
+				currentHeight = arrLookup(data, i, j);
+				arrLookup(visabilityMap, i, j).bot = true;
+			}
+
+			if (currentHeight == 9) {
+				break;
+			}
+		}
+	}
+
+	// left right
+	for (int i = 0; i < length; i++) {
+		int currentHeight = -1;
+
+		for (int j = 0; j < width; j++) {
+			if (arrLookup(data, j, i) > currentHeight) {
+				currentHeight = arrLookup(data, j, i);
+				arrLookup(visabilityMap, j, i).left = true;
+			}
+
+			if (currentHeight == 9) {
+				break;
+			}
+		}
+	}
+
+	auto countVisible = std::ranges::count_if(visabilityMap, [](auto v) {
+		return v.top || v.right || v.bot || v.left;
+	});
+
+	auto end = std::chrono::high_resolution_clock::now();
+
+	fmt::print("Part 1, count visible: {}\n", countVisible);
+
+
+	auto dur = end - start;
+	fmt::print("Took {}\n", dur);
+
+	return 0;
+}
